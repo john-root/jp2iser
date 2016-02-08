07488a54dac8c147e00f3d32b99dd8cdea548c24
Adam Christie <fractos@gmail.com>
2016-02-08 11:16:57 +0000

    uwsgi-ifying tizer, as it is now so-named. fizzzz.

---
+++ tizer.py
@@ -0,0 +1,33 @@
+import jp2iser
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+@app.route("/convert", methods=["POST"])
+def convert():
+
+    data = request.get_json()
+    job_id = data.get("jobid")
+    source = data.get("source")
+    destination = data.get("destination")
+    thumb_dir = data.get("thumbDir")
+    if thumb_dir and thumb_dir[-1] != "/":
+        thumb_dir += "/"
+    thumb_sizes = data.get("thumbSizes")
+
+    # TODO check rest for Noneness
+    if source is not None:
+        # currently no idea how this really went as no return value
+        result = jp2iser.process(source, destination=destination, bounded_sizes=thumb_sizes, bounded_folder=thumb_dir)
+        result["status"] = "Success"
+    else:
+        result = {"status": "Job failed"}
+
+    result["job_id"] = job_id
+
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(threaded=True, debug=True)
